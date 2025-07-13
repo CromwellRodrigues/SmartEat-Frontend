9ef24161d745a585f0e4bc24a0453e418b34b9ed
daffodil009 <daffodil.luck9@gmail.com>
2025-07-13 16:07:24 +0100

    Fix font usage in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${poppins.variable} antialiased`}>
+        <body className={`${poppins.className} antialiased`}>
 
         <Header />  
           {children}
